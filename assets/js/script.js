cae3dbd8a9e3f3f2378071a94f878a45e816757b
MadChazo <141524368+MadChazo@users.noreply.github.com>
2023-09-28 17:40:37 -0400

    Add functionality to put weather data on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,67 @@
 var searchBtn = document.getElementById("search-button");
+var clearBtn = document.getElementById("clear");
 var citySearch = document.getElementById("city-search");
 var historyList = document.getElementById("history");
+var todayCity = document.getElementById("today-city");
+var todayIcon = document.getElementById("today-icon");
+var todayStats = document.getElementById("today-stats");
+var fiveDays = document.getElementsByClassName("5-day");
 
 var cityHistory = JSON.parse(localStorage.getItem("history")) || [];
+
+function reformatDate(date) {
+  var dateArray = date.split("-");
+  var newDate = dateArray[1] + "/" + dateArray[2] + "/" + dateArray[0];
+  return newDate;
+}
+
+function printWeather(data) {
+  var city = data.city.name;
+  var todayDate = reformatDate(data.list[0].dt_txt.split(" ")[0]);
+  var todayWeather = {
+    temp: data.list[0].main.temp,
+    wind: data.list[0].wind.speed,
+    humidity: data.list[0].main.humidity,
+  };
+  todayCity.textContent = city + " (" + todayDate + ")";
+  todayIcon.src =
+    "https://openweathermap.org/img/wn/" +
+    data.list[0].weather[0].icon +
+    "@2x.png";
+  todayStats.innerHTML =
+    "<li>Temp: " +
+    todayWeather.temp +
+    " &deg;F</li><li>Wind: " +
+    todayWeather.wind +
+    " MPH</li><li>Humidity: " +
+    todayWeather.humidity +
+    "%</li>";
+  for (let i = 0; i < fiveDays.length; i++) {
+    var thisDate = fiveDays[i].children[0];
+    var thisIcon = fiveDays[i].children[1];
+    var thisStats = fiveDays[i].children[2];
+    var weatherIndex = 7 + 8 * i;
+    var date = reformatDate(data.list[weatherIndex].dt_txt.split(" ")[0]);
+    var weather = {
+      temp: data.list[weatherIndex].main.temp,
+      wind: data.list[weatherIndex].wind.speed,
+      humidity: data.list[weatherIndex].main.humidity,
+    };
+    thisDate.textContent = date;
+    thisIcon.src =
+      "https://openweathermap.org/img/wn/" +
+      data.list[weatherIndex].weather[0].icon +
+      "@2x.png";
+    thisStats.innerHTML =
+      "<li>Temp: " +
+      weather.temp +
+      " &deg;F</li><li>Wind: " +
+      weather.wind +
+      " MPH</li><li>Humidity: " +
+      weather.humidity +
+      "%</li>";
+  }
+}
 
 function weatherFinder(lat, lon) {
   var requestURL =
@@ -10,20 +69,25 @@ function weatherFinder(lat, lon) {
     lat +
     "&lon=" +
     lon +
-    "&appid=218343ff3f62251dde68dd390ad7ac64";
+    "&units=imperial&appid=218343ff3f62251dde68dd390ad7ac64";
   fetch(requestURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      printWeather(data);
     });
 }
 
 function createHistory(city) {
-  var historyBtn = document.createElement("button");
-  historyBtn.textContent = city;
+  var historyBtn = document.createElement("li");
+  historyBtn.innerHTML = "<button>" + city + "</button>";
+  historyBtn.classList.add("historyBtn");
   historyList.appendChild(historyBtn);
+  historyBtn.addEventListener("click", function () {
+    citySearch.value = city;
+    searchHandler();
+  });
 }
 
 function searchHandler() {
@@ -33,7 +97,6 @@ function searchHandler() {
     cityInput +
     "&limit=5&appid=218343ff3f62251dde68dd390ad7ac64";
   if (!cityInput) {
-    console.log("no input");
     return;
   }
   fetch(requestURL)
@@ -56,8 +119,15 @@ function searchHandler() {
     });
 }
 
+function clearHandler() {
+  localStorage.clear();
+  historyList.innerHTML = "";
+  citySearch.value = "";
+}
+
 for (let i = 0; i < cityHistory.length; i++) {
   createHistory(cityHistory[i]);
 }
 
 searchBtn.addEventListener("click", searchHandler);
+clearBtn.addEventListener("click", clearHandler);
